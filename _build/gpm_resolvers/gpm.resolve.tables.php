5bc022d57fe644c98022535f88237dad67c1173b
Jan Peca <pecajan@gmail.com>
2015-03-23 15:50:53 +0100

    GPM 0.11.0

---
+++ _build/gpm_resolvers/gpm.resolve.tables.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Resolve creating db tables
+ *
+ * THIS RESOLVER IS AUTOMATICALLY GENERATED, NO CHANGES WILL APPLY
+ *
+ * @package gitpackagemanagement
+ * @subpackage build
+ */
+
+if ($object->xpdo) {
+    $modx =& $object->xpdo;
+    switch ($options[xPDOTransport::PACKAGE_ACTION]) {
+        case xPDOTransport::ACTION_INSTALL:
+        case xPDOTransport::ACTION_UPGRADE:
+            $modelPath = $modx->getOption('gitpackagemanagement.core_path', null, $modx->getOption('core_path') . 'components/gitpackagemanagement/') . 'model/';
+            $modx->addPackage('gitpackagemanagement', $modelPath);
+
+            $manager = $modx->getManager();
+
+            $manager->createObjectContainer('GitPackage');
+
+            break;
+    }
+}
+
+return true;
--- core/components/gitpackagemanagement/processors/mgr/gitpackage/buildpackage.class.php
+++ core/components/gitpackagemanagement/processors/mgr/gitpackage/buildpackage.class.php
@@ -132,6 +132,8 @@ class GitPackageManagementBuildPackageProcessor extends modObjectProcessor {
         if (!empty($resourcesArray)) {
             $vehicle->addResourceResolver($this->packagePath . '_build/gpm_resolvers', $resourcesArray);
         }
+        
+        $this->addSystemSettings();
 
         $after = $resolver->getAfter();
         foreach($after as $script) {
@@ -140,7 +142,6 @@ class GitPackageManagementBuildPackageProcessor extends modObjectProcessor {
 
         $this->builder->putVehicle($vehicle);
         $this->addMenus();
-        $this->addSystemSettings();
 
         $packageAttributes = array();
 
