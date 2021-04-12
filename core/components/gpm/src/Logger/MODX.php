b922cac85b2393474abc862d5311c7e961401e7d
matdave <mat@matdave.com>
2021-04-12 11:17:48 -0500

    add file logging option

--- core/components/gpm/src/Logger/MODX.php
+++ core/components/gpm/src/Logger/MODX.php
@@ -41,6 +41,13 @@ final class MODX implements LoggerInterface
             $level = self::$levelMap[$level];
         }
 
-        $this->modx->log($level, $message);
+        $target = (!$this->getOption('enable_file_log')) ? '' : array(
+            'target' => 'FILE',
+            'options' => array(
+                'filename' => 'gpm.log'
+            )
+        );
+
+        $this->modx->log($level, $message, $target);
     }
 }
