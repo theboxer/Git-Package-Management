f16ecb97436f0fe495005e85795b639c9ded5236
Mat Dave Jones <mat@matdave.com>
2026-09-18 10:22:16 -0500

    feat: Add basic search field to packages grid (#187)

--- core/components/gpm/src/Processors/GitPackage/GetList.php
+++ core/components/gpm/src/Processors/GitPackage/GetList.php
@@ -4,6 +4,7 @@ namespace GPM\Processors\GitPackage;
 use GPM\Model\GitPackage;
 use MODX\Revolution\Processors\Model\GetListProcessor;
 use xPDO\Om\xPDOObject;
+use xPDO\Om\xPDOQuery;
 
 class GetList extends GetListProcessor
 {
@@ -18,5 +19,22 @@ class GetList extends GetListProcessor
         $ta = $object->toArray();
         $ta['updatedon'] = ($ta['updatedon']) ? date('Y-m-d H:i:s', $ta['updatedon']) : '';
         return $ta;
+    }
+
+    public function prepareQueryBeforeCount(xPDOQuery $c)
+    {
+        $c = parent::prepareQueryBeforeCount($c);
+
+        $query = $this->getProperty('query');
+        if (!empty($query)) {
+            $c->where([
+                'name:LIKE' => "%{$query}%",
+                'OR:description:LIKE' => "%{$query}%",
+                'OR:author:LIKE' => "%{$query}%",
+                'OR:version:LIKE' => "%{$query}%",
+            ]);
+        }
+
+        return $c;
     }
 }
